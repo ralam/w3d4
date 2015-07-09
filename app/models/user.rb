eb84bc5f7981d263e77a4a17306688bdb8c5c8a3
App Academy Student <student@appacademy.io>
2015-07-09 15:03:16 -0700

    Start completed polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,38 @@ class User < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  def completed_polls
+    responses_from_user = self
+      .responses
+      .select("responses.*")
+      .joins("JOIN responses ON #{self.id} = responses.user_id")
+      # .where("#{self.id} = responses.user_id")
+    # polls = self
+    #   .polls p
+    #   .select("p.*")
+    #   .joins("questions q ON q.poll_id = p.id")
+    #   .joins("answer_choices a ON a.question_id = q.id")
+    #   .joins("LEFT JOIN responses_from_user r ON r.answer_id = a.id")
+    #   .having("COUNT(q.id) != COUNT(r.id)")
+    #   .group("p.id")
+    # Poll.find_by_sql(<<-SQL, id)
+    #   SELECT p.*
+    #   FROM polls p
+    #   JOIN questions q ON q.poll_id = p.id
+    #   JOIN answer_choices a ON a.question_id = q.id
+    #   LEFT JOIN (
+    #     SELECT
+    #       responses.*
+    #     FROM
+    #       responses
+    #     WHERE
+    #       responses.user_id = ?
+    #   ) ON responses.answer_id = a.id
+    #   GROUP BY
+    #     p.id
+    #   HAVING
+    #     COUNT(q.id) != COUNT(responses.id)
+    # SQL
+  end
 end
